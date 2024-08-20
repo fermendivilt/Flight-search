faea6cf4905ec59133f80132a9b2c11708778a46
Fernando Mendivil Terminel <fernando.mendivil@encora.com>
2024-08-20 15:56:35 -0700

    Added page style and Search component

--- front/front/src/App.tsx
+++ front/front/src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Container,
+  Paper,
+  Typography,
+} from "@mui/material";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import Search from "./search/Search";
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fixed sx={{ paddingY: 16 }}>
+      <Typography variant="h3" gutterBottom align="center">
+        Flight Search
+      </Typography>
+
+      <Paper elevation={24} sx={{ padding: 16 }}>
+        <Search />
+      </Paper>
+    </Container>
   );
 }
 
