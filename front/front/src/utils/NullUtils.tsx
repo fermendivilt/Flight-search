2c18c8dad11f1ef7d15f4c1672961475b6c4a799
Fernando Mendivil Terminel <fernando.mendivil@encora.com>
2024-08-30 12:39:06 -0700

    Generic NullUtils

--- front/front/src/utils/NullUtils.tsx
+++ front/front/src/utils/NullUtils.tsx
@@ -1,22 +1,22 @@
 const IsNull = (value: any) => {
-    return value === null;
-}
+  return value === null;
+};
 
-//I hate that ','; stackoverflow says nothing we can do 
-const NotNullString = (value: string | null): string => {
-    if(value === null) return "";
+//I hate that ','; stackoverflow says nothing we can do
+const NotNullString = <T,>(value: T | null): string => {
+  if (IsNull(value)) return "";
 
-    return value;
-}
-const NotNullBoolean = (value: boolean | null): boolean => {
-    if(value === null) return false;
-        
-    return value;
-}
-const NotNullNumber = (value: number | null): number => {
-    if(value === null) return 0;
+  return String(value);
+};
+const NotNullBoolean = <T,>(value: T | null): boolean => {
+  if (IsNull(value)) return false;
 
-    return value;
-}
+  return Boolean(value);
+};
+const NotNullNumber = <T,>(value: T | null): number => {
+  if (IsNull(value)) return 0;
 
-export { IsNull, NotNullString, NotNullBoolean, NotNullNumber }
+  return Number(value);
+};
+
+export { IsNull, NotNullString, NotNullBoolean, NotNullNumber };
