4cdc36b044f2b5724787378a88f5c114c5eccacb
Fernando Mendivil Terminel <fernando.mendivil@encora.com>
2024-09-05 12:30:13 -0700

    Allowed front-end cross origin

--- back/FlightSearch/src/main/java/dev/fermendivilt/FlightSearch/controller/FlightController.java
+++ back/FlightSearch/src/main/java/dev/fermendivilt/FlightSearch/controller/FlightController.java
@@ -1,20 +1,17 @@
 package dev.fermendivilt.FlightSearch.controller;
 
-import dev.fermendivilt.FlightSearch.dto.SearchDTO;
-import dev.fermendivilt.FlightSearch.dto.SearchResponseDTO;
 import dev.fermendivilt.FlightSearch.dto.AirportSearchResponseDTO;
+import dev.fermendivilt.FlightSearch.dto.SearchDTO;
 import dev.fermendivilt.FlightSearch.dto.amadeus.FlightSearchResponseDTO;
 import dev.fermendivilt.FlightSearch.enums.Currency;
 import dev.fermendivilt.FlightSearch.service.FlightService;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.io.SyncFailedException;
@@ -24,12 +21,16 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/flight")
+@CrossOrigin(origins = "http://localhost:3000")
 public class FlightController {
     @Autowired
     FlightService flightService;
 
     @GetMapping("/airports")
-    public ResponseEntity<List<AirportSearchResponseDTO>> searchAirports(@RequestParam @NotBlank String keyword) {
+    public ResponseEntity<List<AirportSearchResponseDTO>> searchAirports(
+        @RequestParam @NotBlank
+        @Pattern(regexp = "^[A-Za-z0-9./:()'\"-]+$")
+        String keyword) {
         try {
             return ResponseEntity.ok(flightService.getAirports(keyword));
 
