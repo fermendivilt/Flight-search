8cbff0c25b48fc747c2ede65be56292132b4dbf5
Fernando Mendivil Terminel <fernando.mendivil@encora.com>
2024-09-05 12:53:34 -0700

    Search page functionality [done]

--- front/front/src/search/Search.tsx
+++ front/front/src/search/Search.tsx
@@ -8,8 +8,17 @@ import {
   Typography,
 } from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2";
-import { ChangeEvent, FormEvent, useState } from "react";
-import { Currencies, SearchDTO, SearchDTOValidation, ValidateSearchDTO } from "../dto/SearchDTO";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import {
+  Currencies,
+  SearchDTO,
+  SearchDTOValidation,
+  ValidateSearchDTO,
+} from "../dto/SearchDTO";
+import { useGetAirports } from "../requester/Requester";
+import { ListElement } from "../dto/SelectListDTO";
+import AutocompleteInput, { OptionList } from "../components/AutocompleteInput";
+import FastSnackbar from "../components/Snackbar";
 
 interface SearchProps {
   search: SearchDTO;
@@ -17,35 +26,28 @@ interface SearchProps {
   moveToResults: () => void;
 }
 
-interface Select {
-  name: string;
-  code: string;
-}
-
-const airportDB: Array<Select> = [
-  { name: "Aeropuerto internacional de Guadalajara", code: "GDL" },
-  { name: "Aeropuerto de Cajeme", code: "CJM" },
-  { name: "Aeropuerto internacional de Monterrey", code: "MTY" },
+const currencyList: Array<ListElement> = [
+  { id: 0, displayName: "United States Dollar", code: Currencies[0] },
+  { id: 1, displayName: "Mexican Peso", code: Currencies[1] },
+  { id: 2, displayName: "Euro", code: Currencies[2] },
 ];
-
-const currencyList: Array<Select> = [
-  { name: "United States Dollar", code: Currencies[0] },
-  { name: "Mexican Peso", code: Currencies[1] },
-  { name: "Euro", code: Currencies[2] },
-];
-
-const airports = {
-  options: airportDB,
-  getOptionLabel: (option: Select) => option.name,
-};
 
 const currencies = {
   options: currencyList,
-  getOptionLabel: (option: Select) => option.name,
+  getOptionLabel: (option: ListElement) => option.displayName,
 };
 
-export default function Search({ search, setSearch, moveToResults }: SearchProps) {
-  
+export default function Search({
+  search,
+  setSearch,
+  moveToResults,
+}: SearchProps) {
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+
+  const fetchDepartureAirports = useGetAirports("");
+
+  const fetchArrivalAirports = useGetAirports("");
+
   const [validation, setValidation] = useState<SearchDTOValidation>({
     hasError: false,
     departureAirportError: "",
@@ -53,8 +55,25 @@ export default function Search({ search, setSearch, moveToResults }: SearchProps
     departureDateError: "",
     arrivalDateError: "",
     adultsError: "",
-    currencyError: ""
-  })
+    currencyError: "",
+  });
+
+  const [departureAirports, setDepartureAirports] = useState<OptionList>({
+    options: [],
+    getOptionLabel: (option) => option.displayName,
+  });
+  const [arrivalAirports, setArrivalAirports] = useState<OptionList>({
+    options: [],
+    getOptionLabel: (option) => option.displayName,
+  });
+
+  const validateAirportSearch = (
+    value: string,
+    setUrl: React.Dispatch<React.SetStateAction<string>>
+  ) => {
+    if (new RegExp("^[A-Za-z0-9./:()'\"-]+$").test(value)) setUrl(value);
+  };
+
   function validate(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
@@ -62,18 +81,74 @@ export default function Search({ search, setSearch, moveToResults }: SearchProps
 
     setValidation(result);
 
-    if(!result.hasError) {
-      
+    if (!result.hasError) {
       const url: URL = new URL(window.location.href);
-      Object.entries(search).forEach(pair => url.searchParams.set(pair[0], pair[1].toString()));
-      window.history.pushState({}, '', url);
+      Object.entries(search).forEach((pair) =>
+        url.searchParams.set(pair[0], pair[1].toString())
+      );
+      window.history.pushState({}, "", url);
 
       moveToResults();
     }
   }
 
+  useEffect(() => {
+    if (fetchDepartureAirports.isLoading) return;
 
-  const handleDepartureAirportChange = (value: Select | null) => {
+    if (fetchDepartureAirports.response !== undefined) {
+      const response = fetchDepartureAirports.response;
+      setDepartureAirports((prev) => {
+        return {
+          ...prev,
+          options: response.map((x, index) => {
+            return {
+              id: index,
+              displayName: x.name + ", " + x.country,
+              code: x.iataCode,
+            };
+          }),
+        };
+      });
+    }
+
+    if (fetchDepartureAirports.error !== undefined) {
+      setSnackbarMessage(fetchDepartureAirports.error);
+    }
+  }, [
+    fetchDepartureAirports.isLoading,
+    fetchDepartureAirports.response,
+    fetchDepartureAirports.error,
+  ]);
+
+  useEffect(() => {
+    if (fetchArrivalAirports.isLoading) return;
+
+    if (fetchArrivalAirports.response !== undefined) {
+      const response = fetchArrivalAirports.response;
+      setArrivalAirports((prev) => {
+        return {
+          ...prev,
+          options: response.map((x, index) => {
+            return {
+              id: index,
+              displayName: x.name + ", " + x.country,
+              code: x.iataCode,
+            };
+          }),
+        };
+      });
+    }
+
+    if (fetchArrivalAirports.error !== undefined) {
+      setSnackbarMessage(fetchArrivalAirports.error);
+    }
+  }, [
+    fetchArrivalAirports.isLoading,
+    fetchArrivalAirports.response,
+    fetchArrivalAirports.error,
+  ]);
+
+  const handleDepartureAirportChange = (value: ListElement | null) => {
     if (value == null) return;
 
     setSearch((prev) => {
@@ -83,7 +158,7 @@ export default function Search({ search, setSearch, moveToResults }: SearchProps
       };
     });
   };
-  const handleArrivalAirportChange = (value: Select | null) => {
+  const handleArrivalAirportChange = (value: ListElement | null) => {
     if (value == null) return;
 
     setSearch((prev) => {
@@ -118,15 +193,14 @@ export default function Search({ search, setSearch, moveToResults }: SearchProps
   ) => {
     let value: number = Number.parseInt(event.target.value);
 
-
     setSearch((prev) => {
       return {
         ...prev,
-        adults: value
+        adults: value,
       };
     });
   };
-  const handleCurrencyChange = (value: Select | null) => {
+  const handleCurrencyChange = (value: ListElement | null) => {
     if (value == null) return;
 
     setSearch((prev) => {
@@ -146,158 +220,140 @@ export default function Search({ search, setSearch, moveToResults }: SearchProps
   };
 
   return (
-    <form id="searchForm" onSubmit={validate}>
-      <Grid2 container gap={4}>
-        <Grid2 xs={12}>
-          <Typography variant="h3" gutterBottom align="center">
-            Flight Search
-          </Typography>
-        </Grid2>
+    <>
+      <form id="searchForm" onSubmit={validate}>
+        <Grid2 container gap={4}>
+          <Grid2 xs={12}>
+            <Typography variant="h3" gutterBottom align="center">
+              Flight Search
+            </Typography>
+          </Grid2>
 
-        <Grid2 xs={5}>
-          <Typography variant="h5" align="right">
-            <label htmlFor="DepartureAirport">Departure airport</label>
-          </Typography>
-        </Grid2>
-
-        <Grid2 xs={6}>
-          <Autocomplete
-            {...airports}
+          <AutocompleteInput
+            label="Departure airport"
             id="DepartureAirport"
-            selectOnFocus
-            blurOnSelect
-            clearOnBlur
-            handleHomeEndKeys
-            renderInput={(params) => (
-              <TextField {...params} label="Search" variant="outlined" />
-            )}
-            onChange={(_e, value, _r, _d) =>
-              handleDepartureAirportChange(value)
+            loading={fetchDepartureAirports.isLoading}
+            options={departureAirports}
+            handleType={(value: string) =>
+              validateAirportSearch(value, fetchDepartureAirports.setUrl)
             }
+            handleSelect={handleDepartureAirportChange}
+            validationError={validation.departureAirportError}
           />
-          <Typography variant="caption" className="error">
-            {validation.departureAirportError}
-          </Typography>
-        </Grid2>
 
-        <Grid2 xs={5}>
-          <Typography variant="h5" align="right">
-            <label htmlFor="ArrivalAirport">Arrival airport</label>
-          </Typography>
-        </Grid2>
-
-        <Grid2 xs={6}>
-          <Autocomplete
-            {...airports}
+          <AutocompleteInput
+            label="Arrival airport"
             id="ArrivalAirport"
-            selectOnFocus
-            blurOnSelect
-            clearOnBlur
-            handleHomeEndKeys
-            renderInput={(params) => (
-              <TextField {...params} label="Search" variant="outlined" />
-            )}
-            onChange={(_e, value, _r, _d) => handleArrivalAirportChange(value)}
+            loading={fetchArrivalAirports.isLoading}
+            options={arrivalAirports}
+            handleType={(value: string) =>
+              validateAirportSearch(value, fetchArrivalAirports.setUrl)
+            }
+            handleSelect={handleArrivalAirportChange}
+            validationError={validation.arrivalAirportError}
           />
-          <Typography variant="caption" className="error">
-            {validation.arrivalAirportError}
-          </Typography>
+
+          <Grid2 xs={5}>
+            <Typography variant="h5" align="right">
+              <label htmlFor="DepartureDate">Departure date</label>
+            </Typography>
+          </Grid2>
+
+          <Grid2 xs={6}>
+            <Input
+              type="date"
+              fullWidth
+              id="DepartureDate"
+              value={search.departureDate}
+              onChange={handleDepartureDateChange}
+            />
+            <Typography variant="caption" className="error">
+              {validation.departureDateError}
+            </Typography>
+          </Grid2>
+
+          <Grid2 xs={5}>
+            <Typography variant="h5" align="right">
+              <label htmlFor="ArrivalDate">Arrival date</label>
+            </Typography>
+          </Grid2>
+
+          <Grid2 xs={6}>
+            <Input
+              type="date"
+              fullWidth
+              id="ArrivalDate"
+              value={search.arrivalDate}
+              onChange={handleArrivalDateChange}
+            />
+            <Typography variant="caption" className="error">
+              {validation.arrivalDateError}
+            </Typography>
+          </Grid2>
+
+          <Grid2 xs={5}>
+            <Typography variant="h5" align="right">
+              <label htmlFor="adults">Number of adults</label>
+            </Typography>
+          </Grid2>
+
+          <Grid2 xs={6}>
+            <Input
+              type="number"
+              fullWidth
+              id="adults"
+              value={search.adults}
+              onChange={handleAdultsChange}
+            />
+            <Typography variant="caption" className="error">
+              {validation.adultsError}
+            </Typography>
+          </Grid2>
+
+          <Grid2 xs={5}>
+            <Typography variant="h5" align="right">
+              <label htmlFor="Currency">Currency</label>
+            </Typography>
+          </Grid2>
+
+          <Grid2 xs={6}>
+            <Autocomplete
+              {...currencies}
+              id="Currency"
+              selectOnFocus
+              blurOnSelect
+              clearOnBlur
+              handleHomeEndKeys
+              renderInput={(params) => (
+                <TextField {...params} label="Search" variant="outlined" />
+              )}
+              onChange={(_e, value, _r, _d) => handleCurrencyChange(value)}
+            />
+            <Typography variant="caption" className="error">
+              {validation.currencyError}
+            </Typography>
+          </Grid2>
+
+          <Grid2 xs={5}></Grid2>
+
+          <Grid2 xs={6}>
+            <Checkbox onChange={handleNonStopChange} sx={{ paddingLeft: 0 }} />{" "}
+            Non-stop
+          </Grid2>
         </Grid2>
 
-        <Grid2 xs={5}>
-          <Typography variant="h5" align="right">
-            <label htmlFor="DepartureDate">Departure date</label>
-          </Typography>
-        </Grid2>
-
-        <Grid2 xs={6}>
-          <Input
-            type="date"
-            fullWidth
-            id="DepartureDate"
-            value={search.departureDate}
-            onChange={handleDepartureDateChange}
-          />
-          <Typography variant="caption" className="error">
-            {validation.departureDateError}
-          </Typography>
-        </Grid2>
-
-        <Grid2 xs={5}>
-          <Typography variant="h5" align="right">
-            <label htmlFor="ArrivalDate">Arrival date</label>
-          </Typography>
-        </Grid2>
-
-        <Grid2 xs={6}>
-          <Input
-            type="date"
-            fullWidth
-            id="ArrivalDate"
-            value={search.arrivalDate}
-            onChange={handleArrivalDateChange}
-          />
-          <Typography variant="caption" className="error">
-            {validation.arrivalDateError}
-          </Typography>
-        </Grid2>
-
-        <Grid2 xs={5}>
-          <Typography variant="h5" align="right">
-            <label htmlFor="adults">Number of adults</label>
-          </Typography>
-        </Grid2>
-
-        <Grid2 xs={6}>
-          <Input
-            type="number"
-            fullWidth
-            id="adults"
-            value={search.adults}
-            onChange={handleAdultsChange}
-          />
-          <Typography variant="caption" className="error">
-            {validation.adultsError}
-          </Typography>
-        </Grid2>
-
-        <Grid2 xs={5}>
-          <Typography variant="h5" align="right">
-            <label htmlFor="Currency">Currency</label>
-          </Typography>
-        </Grid2>
-
-        <Grid2 xs={6}>
-          <Autocomplete
-            {...currencies}
-            id="Currency"
-            selectOnFocus
-            blurOnSelect
-            clearOnBlur
-            handleHomeEndKeys
-            renderInput={(params) => (
-              <TextField {...params} label="Search" variant="outlined" />
-            )}
-            onChange={(_e, value, _r, _d) => handleCurrencyChange(value)}
-          />
-          <Typography variant="caption" className="error">
-            {validation.currencyError}
-          </Typography>
-        </Grid2>
-
-        <Grid2 xs={5}></Grid2>
-
-        <Grid2 xs={6}>
-          <Checkbox onChange={handleNonStopChange} sx={{ paddingLeft: 0 }} />{" "}
-          Non-stop
-        </Grid2>
-      </Grid2>
-
-      <Box display="flex" justifyContent="end">
-        <Button type="submit" form="searchForm" variant="contained">
-          Search
-        </Button>
-      </Box>
-    </form>
+        <Box display="flex" justifyContent="end">
+          <Button type="submit" form="searchForm" variant="contained">
+            Search
+          </Button>
+        </Box>
+      </form>
+      {snackbarMessage.length > 0 && (
+        <FastSnackbar
+          message={snackbarMessage}
+          onDead={() => setSnackbarMessage("")}
+        />
+      )}
+    </>
   );
 }
