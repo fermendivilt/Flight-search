eb641df87669836a4678f26ac28630a87a850581
Fernando Mendivil Terminel <fernando.mendivil@encora.com>
2024-09-10 09:50:15 -0700

    FlightCosts props interface and string to float

--- front/front/src/components/FlightCosts.tsx
+++ front/front/src/components/FlightCosts.tsx
@@ -2,18 +2,28 @@ import { Stack, Typography } from "@mui/material";
 import { StringToValidCurrency } from "../literals/Currencies";
 import NumberToCurrency from "../utils/NumberToCurrency";
 
-export default function FlightCosts() {
+interface FlightCostsProps {
+  currency: string;
+  totalPrice: string;
+  pricePerTraveler: string;
+}
+
+export default function FlightCosts({
+    currency,
+    totalPrice,
+    pricePerTraveler
+}: FlightCostsProps) {
   return (
     <Stack spacing={1} sx={{ marginX: 4 }}>
       <Stack sx={{ alignItems: "flex-end" }}>
         <Typography variant="body1">
-          {NumberToCurrency(StringToValidCurrency("USD"), 1500)}
+          {NumberToCurrency(StringToValidCurrency(currency), parseFloat(totalPrice))}
         </Typography>
         <Typography variant="body2">Total</Typography>
       </Stack>
       <Stack sx={{ alignItems: "flex-end" }}>
         <Typography variant="body1">
-          {NumberToCurrency(StringToValidCurrency("USD"), 500)}
+          {NumberToCurrency(StringToValidCurrency(currency), parseFloat(pricePerTraveler))}
         </Typography>
         <Typography variant="body2">Per traveler</Typography>
       </Stack>
